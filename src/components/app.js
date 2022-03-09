16bb0fed3df2a2550dee8c5bfaf7096a62ec3cc0
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-03-10 03:45:49 +0600

    add functional component + hooks

--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,8 @@
 import React, { PureComponent } from 'react';
+import Restaurants from './restaurants';
 
 export default class App extends PureComponent {
-  state = { value: 0, foo: 'bar' };
-
-  componentDidMount() {
-    console.log('I mounted!');
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('I updated!');
-  }
-
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if (
-  //     nextState.value === this.state.value &&
-  //     nextState.foo === this.state.foo
-  //   ) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
-
-  handleButtonClick = () => {
-    this.setState({ value: this.state.value + 1, foo: 'bar' });
-  };
-
   render() {
-    return (
-      <div>
-        <h1>Hello from App!</h1>
-        <p>{this.state.value}</p>
-        <button onClick={this.handleButtonClick}>+</button>
-      </div>
-    );
+    return <Restaurants restaurants={this.props.restaurants} />;
   }
 }
