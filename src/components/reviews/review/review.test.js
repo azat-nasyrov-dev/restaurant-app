0ec8482abe5c4445c24d9f54234029af4de72531
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-03-14 21:24:57 +0600

    add Reviews tests

---
+++ src/components/reviews/review/review.test.js
@@ -0,0 +1,44 @@
+import { mount } from 'enzyme';
+import Review from './review';
+
+import { restaurants } from '../../../fixtures';
+
+const review = restaurants[0].reviews[1];
+
+const render = (data = {}) => mount(<Review {...data} />);
+
+describe('Review', () => {
+  let reviewsCount, name, text, rate;
+
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    const view = render(review);
+    reviewsCount = view.find('Review').length;
+    name = view.find('[data-id="review-user"]').text();
+    text = view.find('[data-id="review-text"]').text();
+    rate = view.find('svg[data-id="full-star"]').length;
+  });
+
+  it('should render review', () => {
+    expect(reviewsCount).toBe(1);
+  });
+
+  it('should render user name', () => {
+    expect(name).toBe(review.user);
+  });
+
+  it('should render text', () => {
+    expect(text).toBe(review.text);
+  });
+
+  it(`should render ${review.rating} fulled stars`, () => {
+    expect(rate).toBe(review.rating);
+  });
+});
+
+describe('Anonymous Review', () => {
+  it('should render anonymous name', () => {
+    const view = render({ rating: 1 });
+    expect(view.find('[data-id="review-user"]').text()).toBe('Anonymous');
+  });
+});
---
+++ src/components/reviews/reviews.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Reviews from './reviews';
+
+import { restaurants } from '../../fixtures';
+
+const reviews = restaurants[0].reviews;
+
+describe('Reviews', () => {
+  it('should render reviews', () => {
+    const wrapper = mount(<Reviews reviews={reviews} />);
+    expect(wrapper.find('[data-id="review"]').length).toBe(2);
+  });
+});
