b6b0eb7d6a13f94e34a6c4d4e31ab9a78a6e0a4a
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-03-17 00:28:00 +0600

    rename reducers to reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
 
-import reducers from './reducers';
+import reducer from './reducer';
 
-export default createStore(reducers);
+export default createStore(reducer);
