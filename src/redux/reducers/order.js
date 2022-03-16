ef25abb258c35d6aa9e6a38bfe960d3cd1ccf85a
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-03-16 23:31:53 +0600

    fix order bug

--- src/redux/reducers/order.js
+++ src/redux/reducers/order.js
@@ -7,7 +7,7 @@ export default (state = {}, action) => {
     case INCREMENT:
       return { ...state, [id]: (state[id] || 0) + 1 };
     case DECREMENT:
-      return { ...state, [id]: state[id] < 1 ? 0 : (state[id] || 0) - 1 };
+      return { ...state, [id]: state[id] > 0 ? (state[id] || 0) - 1 : 0 };
     case REMOVE:
       return { ...state, [id]: 0 };
     default:
